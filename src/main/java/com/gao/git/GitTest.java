28786fa2fded090d1ce604be7701ca3848916b05
glk <glk@qq.com>
2023-01-12 19:25:22 +0800

    first commit

---
+++ src/main/java/com/gao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.gao.git;
+
+/**
+ * @author 高隆康
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
