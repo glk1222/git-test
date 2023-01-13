b05c7347944150b52eb197e9455fa1bbe8f6f466
glk <glk@qq.com>
2023-01-13 14:15:28 +0800

    push commit

--- src/main/java/com/gao/git/GitTest.java
+++ src/main/java/com/gao/git/GitTest.java
@@ -10,7 +10,8 @@ public class GitTest {
         System.out.println("hello git2!!!");
         System.out.println("hello git3!!!");
         System.out.println("hello git4!!!");
-        System.out.println("master hello git4!!!");
-        System.out.println("hot-fix hello git4!!!");
+        System.out.println("master hello git5!!!");
+        System.out.println("hot-fix hello git6!!!");
+        System.out.println("push hello git7!!!");
     }
 }
